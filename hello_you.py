1c2af5917d39c09b48d4f8e3efaa9f9263f66f73
Sam Singer <samuel.singer1@gmail.com>
2018-12-08 14:53:16 -0800

    Ask user for name

---
+++ hello_you.py
@@ -0,0 +1,14 @@
+# Ask user for name
+print('Hello, what is your name?')
+name = input();
+print('Nice to meet you ' + name)
+
+# Ask user for age
+
+# Ask user for city
+
+# Ask user what they enjoy
+
+# Create output text
+
+# Print output to screen
