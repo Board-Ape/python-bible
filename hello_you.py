c672dfac5b215a13c9b22c4caa17b6e177827506
Sam Singer <samuel.singer1@gmail.com>
2018-12-08 15:03:00 -0800

    This input function is very straight foward

--- hello_you.py
+++ hello_you.py
@@ -1,14 +1,20 @@
 # Ask user for name
 print('Hello, what is your name?')
-name = input();
-print('Nice to meet you ' + name)
+name = input()
 
 # Ask user for age
+print('How old are you?')
+age = input()
 
 # Ask user for city
+print('Which city do you reside in?')
+city = input()
 
 # Ask user what they enjoy
+print('What activity do you enjoy doing?')
+activity = input()
 
 # Create output text
+print(name + ' is ' + age + ' years old, lives in ' + city + ' and enjoys ' + activity)  
 
 # Print output to screen
