74edb48efeb039e1be42f1589c7589d4aaba0612
Sam Singer <samuel.singer1@gmail.com>
2018-12-08 14:44:42 -0800

    Hello world

---
+++ math_modules.py
@@ -0,0 +1,21 @@
+import math
+
+num1 = round(2.1)
+num2 = math.ceil(2.1)
+num3 = math.floor(2.1)
+
+print(num1, num2, num3)
+
+num4 = math.pi
+num5 = math.e
+
+print(num4, num5)
+
+
+num6 = math.sin(math.pi/2)
+num7 = math.sin(math.pi)
+num8 = math.floor(math.sin(math.pi))
+
+# Many powerful mathemetic functions within this Math library
+
+print(num6, num7, num8)
